9c08cfc4a6becff831d393adf5ae42b446d2a36c
cousseaualban <cousseaualban@gmail.com>
2026-02-17 15:52:23 +0100

    app.js modifié -> élément de sécurité HELMET & CORS ajouté

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+import cors from 'cors';
 import { testConnection } from './config/db.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(helmet());
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(express.json());
 
