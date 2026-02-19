3b168cb0afdcc6b3edcd083b447af9c2938a6c58
ILEFB <ilef.belaed@ynov.com>
2026-02-19 15:54:20 +0100

    Recover productModel.js from origin/master

---
+++ backend/src/models/productModel.js
@@ -0,0 +1,24 @@
+import pool from '../config/db.js';
+
+export const createProduct = async (product) => {
+  const [result] = await pool.execute(
+    'INSERT INTO products (label, description, price, category) VALUES (?, ?, ?, ?)',
+    [product.label, product.description, product.price, product.category]
+  );
+  return result.insertId;
+};
+
+export const getAllProduct = async () => {
+  const [rows] = await pool.execute(
+    'SELECT * FROM products'
+  );
+  return rows;
+};
+
+export const getProductById = async (id) => {
+  const [rows] = await pool.execute(
+    'SELECT * FROM products WHERE id = ?',
+    [id]
+  );
+  return rows[0];
+};
